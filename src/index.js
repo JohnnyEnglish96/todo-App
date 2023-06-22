777556128f55e237c89b8c096481205fb6d923a5
Элёр Гайбназаров <127431091+JohnnyEnglish96@users.noreply.github.com>
2023-06-22 19:08:21 +0500

    Delete src directory

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import TodoApp from './components/todo-app/todo-app';
-
-import './index.css';
-
-const root = ReactDOM.createRoot(document.querySelector('#root'));
-root.render(<TodoApp />);
